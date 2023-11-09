804565fefe364a162c9e72e886bfd1b7b97e8a46
Mohammad Akbar <ma63768481@gmail.com>
2023-11-09 17:42:26 +0530

    crede

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -24,18 +24,19 @@ export const authOptions: NextAuthOptions = {
           placeholder: "password",
         },
       },
-      authorize: async (credentials) => {
-        if (
-          credentials?.email === "admin@example.com" &&
-          credentials.password === "admin"
-        ) {
-          const user: User = {
-            id: 1,
-            email: "admin@example.com",
-          };
-          return Promise.resolve(user);
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-        return Promise.resolve(null);
       },
     }),
     GitHubProvider({
