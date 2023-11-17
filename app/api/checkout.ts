60aacbcb1e6fc698e97b2cbf43f9d379b35b7964
Mohammad Akbar <ma63768481@gmail.com>
2023-11-17 16:57:09 +0530

    update

--- app/api/checkout.ts
+++ app/api/checkout.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// import { NextApiRequest, NextApiResponse } from "next";
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const [items, email] = req.body;
-  const modifiedItems = items.map((item) => {});
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const [items, email] = req.body;
+//   const modifiedItems = items.map((item) => {});
+// }
